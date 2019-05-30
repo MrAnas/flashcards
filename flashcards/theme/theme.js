5da0c18aec86d55878fc99ba906221e649c63215
Anas AlBassam <anas2006101@gmail.com>
2019-05-30 07:00:51 +0300

    applying theme

---
+++ flashcards/theme/theme.js
@@ -0,0 +1,10 @@
+export const primary = '#eb6123';
+export const secondary = '#b93fb3';
+
+// Formal Colors
+export const gray = '#757575';
+export const white = '#fff';
+export const black = '#000';
+export const red = '#b71845';
+export const blue = '#4e4cb8';
+export const green = '#6fc23a';
---
+++ flashcards/views/Home.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux';
+import { white, primary, gray } from '../theme/theme'
+
+class Home extends Component {
+    render(){
+        const cardsNumber = this.props.decks[this.props.title].questions.length;
+        return (
+            <View style={styles.deck}>
+                <Text style={styles.deckTitle}>{this.props.title}</Text>
+                <Text style={styles.questions}>{cardsNumber} cards</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    deck: {
+        flexDirection: 'column',
+        marginTop: 5,
+        height: 65,
+        backgroundColor: white,
+        justifyContent: 'left',
+        alignItems: 'left',
+    },
+    deckTitle: {
+        fontSize: 20,
+        color: black,
+        textAlign: 'left',
+    },
+    questions: {
+        textAlign: 'left',
+        fontSize: 16,
+        color: gray,
+    },
+})
+
+function mapStateToProps (state) {
+
+    return {
+        decks: state
+    }
+}
+
+export  default connect(
+    mapStateToProps
+)(Home)
